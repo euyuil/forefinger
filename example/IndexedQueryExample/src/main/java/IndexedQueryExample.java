85c87909cfdbf79d8b02f788f06d8e45959630f9
Liu Yue <euyuil@gmail.com>
2014-05-11 20:52:10 +0800

    Finishing...

---
+++ example/IndexedQueryExample/src/main/java/IndexedQueryExample.java
@@ -0,0 +1,10 @@
+/**
+ * @author Liu Yue
+ * @version 0.0.2014.05.11
+ */
+public class IndexedQueryExample {
+
+    public static void main(String... args) {
+
+    }
+}
--- src/main/java/com/euyuil/forefinger/mapreduce/SimpleViewMapReduce.java
+++ src/main/java/com/euyuil/forefinger/mapreduce/SimpleViewMapReduce.java
@@ -1,5 +1,6 @@
 package com.euyuil.forefinger.mapreduce;
 
+import com.euyuil.forefinger.mapreduce.lib.input.FileSplitInputFormat;
 import com.euyuil.forefinger.meta.MetaData;
 import com.euyuil.forefinger.meta.MetaDataColumn;
 import com.euyuil.forefinger.meta.MetaDataSet;
@@ -11,29 +12,74 @@ import com.euyuil.forefinger.serde.DataRow;
 import com.euyuil.forefinger.serde.Deserializer;
 import com.euyuil.forefinger.serde.Serializer;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Just project something with some filter.
  *
  * Input view could be a very SimpleViewMetaData.
+ * Data source should not be heterogeneous.
  *
  * @author Liu Yue
  * @version 0.0.2014.03.27
  */
 public abstract class SimpleViewMapReduce extends ForefingerMapReduce {
 
-    public static final String PARAM_SIMPLE_VIEW_NAME = "forefinger.simple.view.name";
+    public static boolean startAndWaitForSimpleViewWithIndex(
+            String simpleViewName,
+            String sourcePath,
+            String lookupResultPath,
+            String outputPath) throws IOException, ClassNotFoundException, InterruptedException {
 
-    public static boolean startAndWaitForSimpleViewJob(SimpleViewMetaData simpleViewMetaData) {
-        return false;
+        Configuration configuration = new Configuration();
+
+        configuration.set(PARAM_SIMPLE_VIEW_NAME, simpleViewName);
+
+        Job job = new Job(configuration);
+
+        job.setJarByClass(SimpleViewMapReduce.class);
+
+        job.setMapperClass(SimpleViewMapper.class);
+        job.setMapOutputKeyClass(LongWritable.class);
+        job.setMapOutputValueClass(Text.class);
+
+        job.setReducerClass(SimpleViewReducer.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
+
+        job.setInputFormatClass(FileSplitInputFormat.class);
+
+        FileSystem fileSystem = FileSystem.get(configuration);
+        InputStream inputStream = fileSystem.open(new Path(lookupResultPath));
+        Scanner scanner = new Scanner(inputStream);
+        Path src = new Path(sourcePath);
+        long blockSize = fileSystem.getDefaultBlockSize(src);
+        while (scanner.hasNext()) {
+            String str = scanner.next();
+            long pos = Long.valueOf(str);
+            FileSplitInputFormat.addFileSplit(job, new FileSplitInputFormat.FileSplitDescriptor(src, pos, blockSize));
+        }
+        scanner.close();
+        inputStream.close();
+
+        TextOutputFormat.setOutputPath(job, new Path(outputPath));
+
+        return job.waitForCompletion(true);
     }
+
+    public static final String PARAM_SIMPLE_VIEW_NAME = "forefinger.simple.view.name";
 
     public static SimpleViewMetaData getSimpleViewMetaData(Configuration configuration) {
         String name = configuration.get(PARAM_SIMPLE_VIEW_NAME);
